7b621bc92893fcf4327a5bc685a8749c16c5dc07
‘renxiaotu <lenzhifeng@163.com>
2020-05-01 18:14:23 +0800

    1.0.0

--- src/RedisServer.php
+++ src/RedisServer.php
@@ -8,7 +8,13 @@
 namespace Renxiaotu\database;
 
 class RedisServer extends \Redis{
+    private static function redis(){
+        static $redis;
+        if(!$redis)$redis=new RedisServer();
+        return $redis;
+    }
+
     public static function __callStatic($method, $params){
-        return call_user_func_array([$this, $method], $params);
+        return call_user_func_array([self::redis(), $method], $params);
     }
 }
